c5fbce7d695181163fc01db066533537958ef10c
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-20 11:02:49 +0300

    ХэшСет просто перезаписывает данные, если они были добавлены повторно

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         strings.add("Второе");
         strings.add("Третье");
         strings.add("Первое");
+        strings.add("Второе");
 
         for(String s : strings){
             System.out.println(s);
