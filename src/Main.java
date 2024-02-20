f024ad521bb5708e039f030b1974cd84641294d8
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-20 11:08:33 +0300

    TreeSet хранит элементы в порядке возрастания

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        Set<String> strings = new HashSet<>();
-        strings.add("Первое");
-        strings.add("Второе");
-        strings.add("Третье");
-        strings.add("Первое");
-        strings.add("Второе");
+        Set<Integer> strings = new TreeSet<>();
+        strings.add(32);
+        strings.add(10);
+        strings.add(2);
+//        strings.remove("Третье");
 
-        for(String s : strings){
+        for(Integer s : strings){
             System.out.println(s);
         }
     }
