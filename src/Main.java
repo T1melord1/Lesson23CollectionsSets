9381964d8eb4d092209cdeddaec04e9b109cfee0
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-20 10:59:14 +0300

    HashSet не гарантирует порядка вообще, это он напечатают наоборот к примеру, а числа 1 2 3 по порядку

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+public class Main {
+    public static void main(String[] args) {
+        Set<String> strings = new HashSet<>();
+        strings.add("Первое");
+        strings.add("Второе");
+        strings.add("Третье");
+
+        for(String s : strings){
+            System.out.println(s);
+        }
+    }
+
+}
